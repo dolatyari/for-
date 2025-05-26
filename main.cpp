5850801cf4acf92c2da01819832a666cde389578
dolatyari <shahedrostam@gmail.com>
2025-05-26 22:14:32 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"enter a number : ";
+cin>>a;
+cout<<"numbers small than "<<a<<":"<<endl;
+
+  for(int b;b<a;b++)
+  cout<<b<<"\t";
+
+
+    return 0;
+}
